7271dfad80f8116d4f1b8c62a1018e727dd419f5
singhmi4 <singh.michaelanthony@gmail.com>
2020-06-17 16:09:46 +0000

    render data in table as a test

--- client/src/components/ShowChart.js
+++ client/src/components/ShowChart.js
@@ -1,13 +1,16 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import * as d3 from "d3";
 
 const ShowCharts = () => {
 	
+	const [data, setData] = useState([]);
+	
 	const getChartData = async () => {
 		try {
-			const response = await fetch("https://sandbox-ukmlr.run-us-west2.goorm.io/events/hourly");
+			const response = await fetch("https://sandbox-ukmlr.run-us-west2.goorm.io/events/hourly"); // this needs to be updated
       		const jsonData = await response.json();
-			console.log(jsonData);
+			
+			setData(jsonData);
 		} catch (err) {
 			console.error(err);
 		}
@@ -17,9 +20,30 @@ const ShowCharts = () => {
     getChartData();
   }, []);
 	
+	console.log(data);
+	
 	return (
 		<Fragment>
 			<h1 className="text-center mt-5">Charts</h1>
+			<table class="table mt-5 text-center">
+			<thead>
+			  <tr>
+				<th>Date</th>
+				<th>Hour</th>
+				<th>Events</th>
+			  </tr>
+			</thead>
+			<tbody>
+			  
+			  {data.map(data => (
+				<tr key={data.data_id}>
+				  <td>{data.date}</td>
+				  <td>{data.hour}</td>
+				  <td>{data.events}</td>
+				</tr>
+			  ))}
+			</tbody>
+		  </table>
 		</Fragment>
 	)
 }
