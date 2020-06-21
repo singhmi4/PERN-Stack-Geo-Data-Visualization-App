d97aa842e55ae72ad11f9a378579318619d69597
Michael Singh <singh.michaelanthony@gmail.com>
2020-06-21 17:51:07 -0400

    heroku redis fix attempt: destroy redis addon and create new one

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,10 +1,9 @@
 // code adapted from https://blog.logrocket.com/rate-limiting-node-js/
 
 const moment = require('moment')
-const redis = require('redis')
 
 // const redisClient = (process.env.REDIS_URL) ? redis.createClient(process.env.REDIS_URL) : redis.createClient();
-const redisClient = redis.createClient(process.env.REDIS_URL);
+const client = require('redis').createClient(process.env.REDIS_URL);
 
 const WINDOW_SIZE_IN_HOURS = 24
 const MAX_WINDOW_REQUEST_COUNT = 2400
