1779777b527e04812648324378f9d6206bfb8af3
Michael Singh <singh.michaelanthony@gmail.com>
2020-06-21 14:09:18 -0400

    attempt to fix redis to go code

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -15,6 +15,8 @@ module.exports = function (req, res, next) {
       if (process.env.REDISTOGO_URL) {
         const rtg   = require("url").parse(process.env.REDISTOGO_URL);
         redisClient = redis.createClient(rtg.port, rtg.hostname);
+
+        redisClient.auth(rtg.auth.split(":")[1]);
       } else {
         redisClient = redis.createClient();
       }
